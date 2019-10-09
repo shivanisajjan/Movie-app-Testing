0c79e4ebd27c0f1fd225f43d613f7a0688938f08
shivanisajjan <shivanisajjan269@gmail.com>
2019-10-09 19:58:35 +0530

    first commit

--- src/test/java/com/stackroute/userservice/repository/MovieRepositoryTest.java
+++ src/test/java/com/stackroute/userservice/repository/MovieRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -49,7 +50,6 @@ public class MovieRepositoryTest {
     public void testSaveMovieFailure(){
         Movie testMovie = new Movie(4,"john","Based on True Story of John","Hindi");
         movieRepository.save(movie);
-//        Movie fetchMovie = movieRepository.findById(movie.getId()).get();
         Assert.assertNotSame(testMovie,movie);
     }
 
@@ -62,6 +62,62 @@ public class MovieRepositoryTest {
         List<Movie> list = movieRepository.findAll();
         Assert.assertEquals("john1",list.get(0).getTitle());
     }
-
-
+    @Test
+    public void testDeleteById(){
+        Movie u = new Movie(5,"john1","Based on True Story of John part 1","Hindi");
+        movieRepository.save(u);
+        movieRepository.deleteById(5);
+        Boolean b = movieRepository.findAll().isEmpty();
+        Assert.assertEquals(true,b);
+    }
+    @Test
+    public void testExistById(){
+        Movie u = new Movie(5,"john1","Based on True Story of John part 1","Hindi");
+        Movie u1 = new Movie(6,"john2","Based on True Story of John part 2","Hindi");
+        movieRepository.save(u);
+        movieRepository.save(u1);
+        Boolean b= movieRepository.existsById(5);
+        Assert.assertEquals(true,b);
+    }
+    @Test
+    public void testExistByIdFailure(){
+        Movie u = new Movie(5,"john1","Based on True Story of John part 1","Hindi");
+        Movie u1 = new Movie(6,"john2","Based on True Story of John part 2","Hindi");
+        movieRepository.save(u);
+        movieRepository.save(u1);
+        Boolean b= movieRepository.existsById(6);
+        Assert.assertNotEquals(false,b);
+    }
+    @Test
+    public void testDeleteByIdFailure(){
+        Movie u = new Movie(5,"john1","Based on True Story of John part 1","Hindi");
+        movieRepository.save(u);
+        movieRepository.deleteById(5);
+        Boolean b = movieRepository.findAll().isEmpty();
+        Assert.assertNotEquals(false,b);
+    }
+    @Test
+    public void testGetByTitle(){
+        Movie u = new Movie(5,"john1","Based on True Story of John part 1","Hindi");
+        Movie u1 = new Movie(6,"john1","Based on True Story of John part 2","Hindi");
+        movieRepository.save(u1);
+        movieRepository.save(u);
+        List<Movie> m=new ArrayList<>();
+        m.add(u);
+        m.add(u1);
+        List<Movie> movie=movieRepository.findBytitle("john1");
+        Assert.assertEquals(m,movie);
+    }
+    @Test
+    public void testGetByTitleFailure(){
+        Movie u = new Movie(5,"john1","Based on True Story of John part 1","Hindi");
+        Movie u1 = new Movie(6,"john2","Based on True Story of John part 2","Hindi");
+        movieRepository.save(u1);
+        movieRepository.save(u);
+        List<Movie> m=new ArrayList<>();
+        m.add(u);
+        m.add(u1);
+        List<Movie> movie=movieRepository.findBytitle("john1");
+        Assert.assertNotEquals(m,movie);
+    }
 }
